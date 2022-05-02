ae529f096ccca642e45bcd83e668799dd77ed7f3
Ruslan <ri4i23aminov@yandex.ru>
2022-05-02 18:06:57 +0300

    zada4a2 Please enter the commit message for your changes. Lines starting

--- zada4a2/Program.cs
+++ zada4a2/Program.cs
@@ -2,37 +2,36 @@
  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 0,5) */
 
-Console.Write("Введите число k1 = ");
-double k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите число b1 = ");
-double b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите число k2 = ");
-double k2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите число b2 = ");
-double b2 = Convert.ToDouble(Console.ReadLine());
-
-if(k1 == k2)
+double k1 = Input1("k1");
+double b1 = Input1("b1");
+double k2 = Input2("k2");
+double b2 = Input2("b2");
+if (k1 == k2)
 {
-    Console.WriteLine("Прямые параллельны ");
+    Console.WriteLine("Прямые параллельны");
 }
 else
 {
-    double x = (b2 - b1) / (k1 - k2);
-    double y = k2 * (b2 - b1) / (k1 - k2) + b2;
-    Console.WriteLine($"Координаты пересечения прямых {x}, {y} ");
+     double x = ResultX(b2,b1,k1,k2);    
+     double y = ResultY(b2,b1,k1,k2);
+     Console.WriteLine($"Координаты пересечения прямых ({x};{y})");
 }
 
-double ResultX(double a2, double a1, double b1, double b2)
+double ResultX(double a2,double a1, double b1,double b2)
 {
-    return (a2 - a1) / (b1 - b2);
+    return (a2-a1)/(b1-b2);
 }
-
-Resultx(4, 2, 5, 9);
-Console.WriteLine(ResultX(4, 2, 5, 9));
-
-double ResultY(double a2, double a1, double b1, double b2)
+double ResultY(double a2,double a1, double b1,double b2)
 {
-    return b2 * (a2 - a1) / (b1 - b2) + a2;
+    return b2*((a2-a1)/(b1-b2)) +a2;
 }
-Resultx(4, 2, 5, 9);
-Console.WriteLine(ResultY(4, 2, 5, 9));
+double Input1(string k1)
+{
+    Console.Write($"Введите коэффициенты для функции первой прямой {k1} ");
+    return Convert.ToDouble(Console.ReadLine());   
+}
+double Input2(string k2)
+{
+    Console.Write($"Введите коэффициенты для функции первой прямой {k2} ");
+    return Convert.ToDouble(Console.ReadLine()); 
+}
