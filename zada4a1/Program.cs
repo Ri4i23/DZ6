297dc0888608004e61093bed98ba70998d4e38f5
Ruslan <ri4i23aminov@yandex.ru>
2022-05-02 18:04:40 +0300

    zada4a1

---
+++ zada4a1/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3 */
+
+Console.WriteLine("Введите длину массива ");
+int length = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[length];
+
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine($"Введите {i} элемент массива");
+    array[i] = Convert.ToDouble(Console.ReadLine());
+}
+int count = 0;
+for(int i = 0; i<length;i++)
+{
+    if(array[i]>0)
+    {
+        count++; 
+    }
+    else
+    {
+        count = count + 0;
+    }    
+}
+Console.WriteLine($"Количество положительных чисел в массиве = {count} ");
---
+++ zada4a2/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых,
+ заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 0,5) */
+
+Console.Write("Введите число k1 = ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число b1 = ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число k2 = ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число b2 = ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+if(k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны ");
+}
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k2 * (b2 - b1) / (k1 - k2) + b2;
+    Console.WriteLine($"Координаты пересечения прямых {x}, {y} ");
+}
+
+double ResultX(double a2, double a1, double b1, double b2)
+{
+    return (a2 - a1) / (b1 - b2);
+}
+
+Resultx(4, 2, 5, 9);
+Console.WriteLine(ResultX(4, 2, 5, 9));
+
+double ResultY(double a2, double a1, double b1, double b2)
+{
+    return b2 * (a2 - a1) / (b1 - b2) + a2;
+}
+Resultx(4, 2, 5, 9);
+Console.WriteLine(ResultY(4, 2, 5, 9));
